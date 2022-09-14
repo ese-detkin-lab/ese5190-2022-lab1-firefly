7f8f4bcbb941f97839cc1ab37807ce6fde61e911
TomGoh <haozewu@seas.upenn.edu>
2022-09-13 20:42:01 -0400

    Add auto delete chars
    Add auto delete after keyboard output

--- code.py
+++ code.py
@@ -27,8 +27,18 @@ def color(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
 current_lux=0
-control_key = Keycode.ENTER
+enter_key = Keycode.ENTER
+backspace_key=Keycode.BACKSPACE
 
+str_1="Be careful! It getting dark!"
+str_2="All safe, brighter now."
+str_3="An object just climbed up!"
+str_4="An object just fell down!"
+str_5="An object just turned left!"
+str_6="An object just turned right!"
+
+
+time.sleep(6)
 while True:
     # while not apds.color_data_ready:
     #     time.sleep(0.005)
@@ -37,34 +47,58 @@ while True:
     lux=colorutility.calculate_lux(r,g,b)
     pixels.fill((r, g, b))
     if current_lux>lux*1.5:
-        keyboard_layout.write('Be careful! It getting dark!')
-        keyboard.press(control_key)
-        keyboard.release_all()
+        keyboard_layout.write(str_1)
+        # keyboard.press(enter_key)
+        # keyboard.release_all()
+        time.sleep(0.1)
+        for i in range(len(str_1)):
+            keyboard.press(backspace_key)
+            keyboard.release_all()
         current_lux=lux
     elif current_lux<lux/1.5:
-        keyboard_layout.write('All safe, brighter now.')
-        keyboard.press(control_key)
-        keyboard.release_all()
+        keyboard_layout.write(str_2)
+        # keyboard.press(enter_key)
+        # keyboard.release_all()
+        time.sleep(0.1)
+        for i in range(len(str_2)):
+            keyboard.press(backspace_key)
+            keyboard.release_all()
         current_lux=lux
 
 
     gesture = apds.gesture()
     if gesture == 1:
-        keyboard_layout.write('An object just climbed up!')
-        keyboard.press(control_key)
-        keyboard.release_all()
+        keyboard_layout.write(str_3)
+        # keyboard.press(enter_key)
+        # keyboard.release_all()
+        time.sleep(0.1)
+        for i in range(len(str_3)):
+            keyboard.press(backspace_key)
+            keyboard.release_all()
     if gesture == 2:
-        keyboard_layout.write('An object just fell down!')
-        keyboard.press(control_key)
-        keyboard.release_all()
+        keyboard_layout.write(str_4)
+        # keyboard.press(enter_key)
+        # keyboard.release_all()
+        time.sleep(0.1)
+        for i in range(len(str_4)):
+            keyboard.press(backspace_key)
+            keyboard.release_all()
     if gesture == 3:
-        keyboard_layout.write('An object just turned left!')
-        keyboard.press(control_key)
-        keyboard.release_all()
+        keyboard_layout.write(str_5)
+        # keyboard.press(enter_key)
+        # keyboard.release_all()
+        time.sleep(0.1)
+        for i in range(len(str_5)):
+            keyboard.press(backspace_key)
+            keyboard.release_all()
     if gesture == 4:
-        keyboard_layout.write('An object just turned right!')
-        keyboard.press(control_key)
-        keyboard.release_all()
+        keyboard_layout.write(str_6)
+        # keyboard.press(enter_key)
+        # keyboard.release_all()
+        time.sleep(0.1)
+        for i in range(len(str_6)):
+            keyboard.press(backspace_key)
+            keyboard.release_all()
     
 
 
