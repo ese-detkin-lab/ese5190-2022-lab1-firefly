55af9296275ce2ca0a6118917a5a15ade81fcad1
TomGoh <haozewu@seas.upenn.edu>
2022-09-13 21:39:45 -0400

    Add mouse scroll movement.

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
 import time
 import board
-import digitalio
-import analogio
 import usb_hid
 import neopixel
 from adafruit_apds9960.apds9960 import APDS9960
@@ -19,12 +17,9 @@ keyboard = Keyboard(usb_hid.devices)
 keyboard_layout = KeyboardLayoutUS(keyboard)
 
 apds.enable_color = True
-apds.enable_proximity=True
 apds.enable_gesture=True
 
 apds.color_integration_time=0x22
-def color(r, g, b, text):
-    return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
 
 current_lux=0
 enter_key = Keycode.ENTER
@@ -40,26 +35,21 @@ str_6="An object just turned right!"
 
 time.sleep(6)
 while True:
-    # while not apds.color_data_ready:
-    #     time.sleep(0.005)
     
     r, g, b, c = apds.color_data
     lux=colorutility.calculate_lux(r,g,b)
     pixels.fill((r, g, b))
+    
     if current_lux>lux*1.5:
         keyboard_layout.write(str_1)
-        # keyboard.press(enter_key)
-        # keyboard.release_all()
-        time.sleep(0.1)
+        time.sleep(0.4)
         for i in range(len(str_1)):
             keyboard.press(backspace_key)
             keyboard.release_all()
         current_lux=lux
     elif current_lux<lux/1.5:
         keyboard_layout.write(str_2)
-        # keyboard.press(enter_key)
-        # keyboard.release_all()
-        time.sleep(0.1)
+        time.sleep(0.4)
         for i in range(len(str_2)):
             keyboard.press(backspace_key)
             keyboard.release_all()
@@ -67,38 +57,35 @@ while True:
 
 
     gesture = apds.gesture()
+
     if gesture == 1:
         keyboard_layout.write(str_3)
-        # keyboard.press(enter_key)
-        # keyboard.release_all()
-        time.sleep(0.1)
+        mouse.move(wheel=-20)
+        time.sleep(0.4)
+        mouse.move(wheel=20)
         for i in range(len(str_3)):
             keyboard.press(backspace_key)
             keyboard.release_all()
+
     if gesture == 2:
         keyboard_layout.write(str_4)
-        # keyboard.press(enter_key)
-        # keyboard.release_all()
-        time.sleep(0.1)
+        mouse.move(wheel=20)
+        time.sleep(0.4)
+        mouse.move(wheel=-20)
         for i in range(len(str_4)):
             keyboard.press(backspace_key)
             keyboard.release_all()
+
     if gesture == 3:
         keyboard_layout.write(str_5)
-        # keyboard.press(enter_key)
-        # keyboard.release_all()
-        time.sleep(0.1)
+        time.sleep(0.4)
         for i in range(len(str_5)):
             keyboard.press(backspace_key)
             keyboard.release_all()
+
     if gesture == 4:
         keyboard_layout.write(str_6)
-        # keyboard.press(enter_key)
-        # keyboard.release_all()
-        time.sleep(0.1)
+        time.sleep(0.4)
         for i in range(len(str_6)):
             keyboard.press(backspace_key)
             keyboard.release_all()
-    
-
-
