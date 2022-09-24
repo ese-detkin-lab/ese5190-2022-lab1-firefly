61248e99ef566f235c0913a4bfaf492c99b007c5
aamirabbaskhambaty <114267693+aamirabbaskhambaty@users.noreply.github.com>
2022-09-23 20:46:37 -0400

    added Files,Screenshots and GIFs

---
+++ 4.3.py
@@ -0,0 +1,50 @@
+# SPDX-FileCopyrightText: 2018 Kattni Rembor for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+"""CircuitPython Essentials HID Keyboard example"""
+import time
+
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+import adafruit_apds9960.apds9960
+import neopixel
+
+pixels = neopixel.NeoPixel(board.NEOPIXEL, 1, auto_write=True)
+
+# A simple neat keyboard demo in CircuitPython
+
+# The Keycode sent for each button, will be paired with a control key
+keys_pressed = [Keycode.BACKSPACE, "O"]
+control_key = Keycode.SHIFT
+
+# The keyboard object!
+time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)  # We're in the US :)
+
+i2c = board.STEMMA_I2C()
+sensor = adafruit_apds9960.apds9960.APDS9960(i2c)
+sensor.enable_color = True
+ 
+sensor.color_integration_time = 1
+
+print("Waiting for key pin...")
+
+prev_brightness = 0
+
+while True:
+    r, b, g, c = sensor.color_data
+    
+    if c >= prev_brightness:
+        keyboard_layout.write("O")
+        
+    else:
+        keyboard.press(Keycode.SHIFT, Keycode.BACKSPACE)
+    
+    prev_brightness = c
+    time.sleep(0.5)
